7276c8b631a8d7761bc27459e464aa76916b1700
Joar <joards@gmail.com>
2015-08-21 16:41:02 +0200

    Added script. Added example csv file Added *~ and #*# to gitignore

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # python-csv-to-latex-table-script
-Simple script which takes a csv file and outputs a latex table
+Simple script which takes a csv file and outputs a latex table to stdout
+
+
+## Requirements:
+* Python 2.7.5 (or later)
+
+## Usage:
+    $ python csv2latextable.py -i example1.csv
+Writes to stdout a latex table based on example1.csv
+Add the flag "-h" to get more info about user selectable parameters.
+
+```python csv2latextable.py -h
+usage: csv2latextable.py [-h] [-i INPUTFILE] [-d DELIMITER] [-q QUOTECHAR]
+                         [-pos TABLEPOS] [-caption CAPTION] [-lable REFLABLE]
+
+Csv to latex table converter.
+
+optional arguments:
+  -h, --help        show this help message and exit
+  -i INPUTFILE      Csv file to read, default=example1.csv
+  -d DELIMITER      Set csv delimiter, default=;
+  -q QUOTECHAR      Set csv quotechar, default="
+  -pos TABLEPOS     Set table position, default=htbp
+  -caption CAPTION  Set table caption, default='Generated table'
+  -lable REFLABLE   Set table reference lable, default=''
+```
---
+++ csv2latextable.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python
+import argparse
+import csv
+import re
+
+class Csv2LatexTable:
+    def __init__(self, inputFile, delimiter, quotechar, tablepos, cap, reflable):
+        self.inputFile = inputFile
+        self.delimiter = delimiter
+        self.quotechar = quotechar
+        self.tablePos  = tablepos 
+        self.caption   = cap
+        self.refLable  = reflable
+
+    def readCsv(self):
+        with open(self.inputFile, "r") as csvfile:
+            tablereader = csv.reader(csvfile, delimiter=self.delimiter, quotechar=self.quotechar)
+            columnHeaders = tablereader.next()
+            numColumn = len(columnHeaders)
+            self.genTableHead(numColumn)
+            
+            print("  %s \\\\" % (' & '.join(columnHeaders[:numColumn])))
+            print("  \\hline")
+            
+            for row in tablereader:
+                #print(len(row))
+                print("  %s \\\\" % re.sub('_', '\\_', ' & '.join(row[:numColumn]) ))
+            
+            self.genTableFooter()
+
+    def genTableHead(self, numColumn):
+        temp = 'c' + ('|c'*(numColumn-1))
+        print("""\\begin{table}[%s]
+\\centering
+ \\caption{%s}
+ \\begin{tabular}{%s}""" % (self.tablePos, self.caption, temp))
+
+    def genTableFooter(self):
+        print(" \\end{tabular}")
+        if (self.refLable != ""):
+            print("\\label{%s}" % self.refLable)
+        print("\\end{table}")
+
+
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(
+        description="""Csv to latex table converter.  """)
+    parser.add_argument('-i', dest='inputFile', default="example1.csv", 
+                        help="Csv file to read, default=example1.csv")    
+    parser.add_argument('-d', dest='delimiter', default=";", 
+                        help="Set csv delimiter, default=;")    
+    parser.add_argument('-q', dest='quotechar', default='"', 
+                        help='Set csv quotechar, default="')    
+    parser.add_argument('-pos', dest='tablePos', default="htbp", 
+                        help="Set table position, default=htbp")
+    parser.add_argument('-caption', dest='caption', default="Generated table", 
+                        help="Set table caption, default='Generated table'")   
+    parser.add_argument('-lable', dest='refLable', default="", 
+                        help="Set table reference lable, default=''")   
+
+    args = parser.parse_args()
+
+    c2lt = Csv2LatexTable(args.inputFile, args.delimiter, args.quotechar, args.tablePos, args.caption, args.refLable)
+    c2lt.readCsv()
