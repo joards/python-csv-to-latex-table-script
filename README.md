c4894f00ead3506023e2b08878f1969060a6236f
Joar <joards@gmail.com>
2015-09-14 17:15:12 +0200

    Added tablespec option. Added longtable option. Added lessspacing option. Changed underline option to no underlining, i.e. underline is now standard.

--- README.md
+++ README.md
@@ -13,18 +13,23 @@ Add the flag "-h" to get more info about user selectable parameters.
 python csv2latextable.py -h
 usage: csv2latextable.py [-h] [-i INPUTFILE] [-d DELIMITER] [-q QUOTECHAR]
                          [-pos TABLEPOS] [-caption CAPTION] [-lable REFLABLE]
-                         [--underline]
+                         [--nounderline] [-tablespec TABLESPEC] [--longtable]
+                         [--lessspacing]
 
 Csv to latex table converter.
 
 optional arguments:
-  -h, --help        show this help message and exit
-  -i INPUTFILE      Csv file to read, default=example1.csv
-  -d DELIMITER      Set csv delimiter, default=;
-  -q QUOTECHAR      Set csv quotechar, default="
-  -pos TABLEPOS     Set table position, default=htbp
-  -caption CAPTION  Set table caption, default='Generated table'
-  -lable REFLABLE   Set table reference lable, default=''
-  --underline       Add underline for each new entry
+  -h, --help            show this help message and exit
+  -i INPUTFILE          Csv file to read, default=example1.csv
+  -d DELIMITER          Set csv delimiter, default=;
+  -q QUOTECHAR          Set csv quotechar, default="
+  -pos TABLEPOS         Set table position, default=htbp
+  -caption CAPTION      Set table caption, default='Generated table'
+  -lable REFLABLE       Set table reference lable, default=''
+  --nounderline         Don't add underline for each entry
+  -tablespec TABLESPEC  Set table specifications, default='c', takes one type
+                        and repeats it
+  --longtable           Use longtable package
+  --lessspacing         Don't add '&&&\\' as spacings between entries
 ```
 
--- csv2latextable.py
+++ csv2latextable.py
@@ -4,45 +4,111 @@ import csv
 import re
 
 class Csv2LatexTable:
-    def __init__(self, inputFile, delimiter, quotechar, tablepos, cap, reflable, underLine):
+    def __init__(self, inputFile, delimiter, quotechar, tablepos, cap, reflable, noUnderLine, tableSpec, longTable, lessSpacing):
         self.inputFile = inputFile
         self.delimiter = delimiter
         self.quotechar = quotechar
         self.tablePos  = tablepos 
         self.caption   = cap
         self.refLable  = reflable
-        self.underLine = underLine
+        self.noUnderLine = noUnderLine
+        self.tableSpec = tableSpec
+        self.longTable = longTable
+        self.lessSpacing = lessSpacing
 
-    def readCsv(self):
+    def readCsvandMakeTable(self):
+        """Reads a csv file and outputs a table to stdout"""
         with open(self.inputFile, "r") as csvfile:
             tablereader = csv.reader(csvfile, delimiter=self.delimiter, quotechar=self.quotechar)
             columnHeaders = tablereader.next()
             numColumn = len(columnHeaders)
-            self.genTableHead(numColumn)
+            self.genTableHead(numColumn, columnHeaders)
             
-            print("  %s \\\\" % (' & '.join(columnHeaders[:numColumn])))
-            print("  \\hline")
+
+            if (not self.noUnderLine):
+                print("\\hline")
             
             for row in tablereader:
-                #print(len(row))
+                if (not self.lessSpacing):
+                    print('&'*(numColumn-1) + '\\\\')
+                
+                # Replace newlines or comma with actual newline in table
+                for i in range(numColumn-1): #temp fix
+                    if '\n' in row[i]:
+                        row[i] = '\\parbox{0.4\\textwidth}{' + str.replace(row[i], '\n', ' \\\\') + '}'
+                    elif ',' in row[i]:
+                        row[i] = '\\parbox{0.4\\textwidth}{' + str.replace(row[i], ',', ' \\\\') + '}'
+                    
+                if '\n' in row:
+                    print(row.index('\n'))
+                
                 print("  %s \\\\" % re.sub('_', '\\_', ' & '.join(row[:numColumn]) ))
-                if (self.underLine):
+                if (not self.noUnderLine):
                     print("\\hline")
             
             self.genTableFooter()
 
-    def genTableHead(self, numColumn):
-        temp = 'c' + ('|c'*(numColumn-1)) + '|'
-        print("""\\begin{table}[%s]
+
+
+    def longTableHeaders(self, numColumn, columnHeaders):
+        """ Generates the column headers for longtable,
+        function takes number of columns and the column headers"""
+        for i in range(numColumn):
+            joiner = " &"
+            if (i == (numColumn-1)):
+                joiner = " \\\\"
+            print("\\multicolumn{1}{%s|}{\\textbf{%s}} %s" % (self.tableSpec, columnHeaders[i], joiner))
+
+    def genTableHead(self, numColumn, columnHeaders):
+        """Make the table headers,
+        either standard or longtable."""
+        temp = '|' + self.tableSpec + (('|'+self.tableSpec)*(numColumn-1)) + '|'
+        if (not self.longTable):
+            print("""\\begin{table}[%s]
 \\centering
  \\caption{%s}
  \\begin{tabular}{%s}""" % (self.tablePos, self.caption, temp))
+        
+            print("  %s \\\\" % (' & '.join(columnHeaders[:numColumn])))
+            print("  \\hline")
+            
+        else:
+            print("""\\begin{center}
+\\begin{longtable}[%s]{%s}
+\\caption{%s}""" % (self.tablePos, temp, self.caption))
+
+            if (self.refLable != ""):
+                print("\\label{%s}\\\\" % self.refLable)
+                print("\\hline")
+            
+            self.longTableHeaders(numColumn, columnHeaders)
+            print("""\\hline 
+\\endfirsthead
+\\multicolumn{%d}{c}{{\\bfseries \\tablename\\ \\thetable{} -- continued from previous page}} \\\\
+\hline""" % numColumn)
+            self.longTableHeaders(numColumn, columnHeaders)
+            print("""\\hline 
+\\endhead
+
+\\hline \\multicolumn{%d}{|r|}{{Continued on next page}} \\\\ \\hline
+\\endfoot
+
+\\hline \\hline
+\\endlastfoot""" % numColumn)
+
 
     def genTableFooter(self):
-        print(" \\end{tabular}")
-        if (self.refLable != ""):
-            print("\\label{%s}" % self.refLable)
-        print("\\end{table}")
+        """ Prints out table footers """
+        if (not self.longTable):
+            print(" \\end{tabular}")
+            if (self.refLable != ""):
+                print("\\label{%s}" % self.refLable)
+            print("\\end{table}")
+        
+        else:
+            print("\\end{longtable}")
+            print("\\end{center}")
+
 
 
 
@@ -61,10 +127,17 @@ if __name__=="__main__":
                         help="Set table caption, default='Generated table'")   
     parser.add_argument('-lable', dest='refLable', default="", 
                         help="Set table reference lable, default=''")   
-    parser.add_argument('--underline', dest='underLine', action='store_true', 
-                        help="Add underline for each new entry")   
+    parser.add_argument('--nounderline', dest='noUnderLine', action='store_true',
+                        help="Don't add underline for each entry")   
+    parser.add_argument('-tablespec', dest='tableSpec', default="c", 
+                        help="Set table specifications, default='c', takes one type and repeats it")  
+    parser.add_argument('--longtable', dest='longTable', action='store_true',
+                        help="Use longtable package") 
+    parser.add_argument('--lessspacing', dest='lessSpacing', action='store_true',
+                        help="Don't add '&&&\\\\' as spacings between entries")   
+    
 
     args = parser.parse_args()
 
-    c2lt = Csv2LatexTable(args.inputFile, args.delimiter, args.quotechar, args.tablePos, args.caption, args.refLable, args.underLine)
-    c2lt.readCsv()
+    c2lt = Csv2LatexTable(args.inputFile, args.delimiter, args.quotechar, args.tablePos, args.caption, args.refLable, args.noUnderLine, args.tableSpec, args.longTable, args.lessSpacing)
+    c2lt.readCsvandMakeTable()
